aa11b1cc7ef6d7853f4b5d0893a5dda49110ac98
Haley DeKeyser <hcd09@live.com>
2019-05-03 02:27:19 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,21 +1,22 @@
 def division(num1, num2)
- 
+  num1/num2
 end
 
 def assign_variable(value)
-  
+  value = "Bob"
 end
 
-def argue
-  
+def argue(phrase)
+  phrase
 end
 
-def greeting
-
+def greeting(name, occupation)
+  puts "Hello, #{name}! You are a great #{occupation}."
 end
 
 def return_a_value
-
+  word = "Nice"
+  word
 end
 
 def last_evaluated_value
